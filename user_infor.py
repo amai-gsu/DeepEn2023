c86b9b46b0d1c0a7314a880a15d8260eef93c1f7
xiaolong-shawn <31405329+xiaolong-shawn@users.noreply.github.com>
2024-01-04 17:24:52 -0500

    update user survey

--- user_infor.py
+++ user_infor.py
@@ -11,11 +11,12 @@ def submit_survey():
     organization = request.form['Organization']
     reason = request.form['Reason']
 
-    # 调用 user-survey.py 中的函数将数据写入 Google 表格
+    # 调用 user_survey.py 中的函数将数据写入 Google 表格
     update_sheet(name, email, organization, reason)
 
     # 重定向到下载页面
     return redirect('/static/download-link.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)  # 移除 debug=True 或将其设置为 False
+    app.run(host='0.0.0.0', port=5000)  # 添加 host='0.0.0.0' 来允许外部访问
